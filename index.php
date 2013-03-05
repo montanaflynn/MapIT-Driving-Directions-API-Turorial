1d30a00e216d557d61cac4b42a67966fe7002b36
Montana Flynn <montana@montanaflynn.me>
2013-03-04 23:13:17 -0800

    Added detailed comments to all code

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
 <?php
 
 /*******************************************
-* SET UP MASHAPE API CALL
+* MapIT Driving Directions WebApp
+* by http://montanaflynn.me/
 *******************************************/
 
-// Avoid wasting CPU or API calls
+// Avoid wasting CPU or API calls if don't have any requests
 if (!isset($_POST['start_location']) || !isset($_POST['end_location'])) {
 	$abort = true;
 }
 
 // Fire away when ready
 if (!$abort){
+	
+	// Require google maps driving directions mashape library
+	// https://www.mashape.com/montanaflynn/google-maps-driving-directions
 	require_once("mashape/GoogleMapsDrivingDirections.php");
-	$client = new GoogleMapsDrivingDirections('PUBSooO7c4$yX_owkYcsn8CDCV_enfz2');
+	
+	// Instantiate API client with your API key
+	$client = new GoogleMapsDrivingDirections('APIKEY');
+	
+	// Get response from API from the directions endpoint
+	// sending the start and end location as parameters
 	$response = $client->directions($_POST['start_location'], $_POST['end_location']);
 	$directions = $response->body;
 	
-	// Set this for our view
+	// Setting this for our view so we know what to show
 	if ($directions->error) {
 		$abort = true;
 	}
 }
 
-// Output view
+// Output our view file which contains the html and form
 include('view.php');
+
